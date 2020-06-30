3ba82b3026802c3fa71fd62c04b2b690b21ee9c4
MisterHope <jeremy@mbp-de-jeremy.lan>
2020-06-30 23:37:56 +0200

    lothric sword change

--- src/main/java/net/vox/imperialweapons/item/LothricKnightSwordItem.java
+++ src/main/java/net/vox/imperialweapons/item/LothricKnightSwordItem.java
@@ -8,6 +8,8 @@ import net.minecraftforge.registries.ObjectHolder;
 
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.SwordItem;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.IItemTier;
 
@@ -23,7 +25,7 @@ public class LothricKnightSwordItem extends ImperialweaponsModElements.ModElemen
 	public void initElements() {
 		elements.items.add(() -> new SwordItem(new IItemTier() {
 			public int getMaxUses() {
-				return 100;
+				return 300;
 			}
 
 			public float getEfficiency() {
@@ -31,7 +33,7 @@ public class LothricKnightSwordItem extends ImperialweaponsModElements.ModElemen
 			}
 
 			public float getAttackDamage() {
-				return 2f;
+				return 4.4f;
 			}
 
 			public int getHarvestLevel() {
@@ -39,13 +41,13 @@ public class LothricKnightSwordItem extends ImperialweaponsModElements.ModElemen
 			}
 
 			public int getEnchantability() {
-				return 2;
+				return 10;
 			}
 
 			public Ingredient getRepairMaterial() {
-				return Ingredient.EMPTY;
+				return Ingredient.fromStacks(new ItemStack(Items.IRON_INGOT, (int) (1)));
 			}
-		}, 3, -3f, new Item.Properties().group(ImperialWeaponryItemGroup.tab)) {
+		}, 3, -2.4000000000000001f, new Item.Properties().group(ImperialWeaponryItemGroup.tab)) {
 		}.setRegistryName("lothric_knight_sword"));
 	}
 }
